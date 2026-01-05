e30c4c47b6b179e57cf04189e79fec7fd98d80f4
bluestpanda <37978356+khalilFadi@users.noreply.github.com>
2026-01-05 13:01:52 -0700

    adding responses files

---
+++ client/src/main/java/client/model/response/ErrorMessageResponse.java
@@ -0,0 +1,19 @@
+package client.model.response;
+
+public class ErrorMessageResponse {
+    private String message;
+
+    public ErrorMessageResponse() {}
+
+    public ErrorMessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
